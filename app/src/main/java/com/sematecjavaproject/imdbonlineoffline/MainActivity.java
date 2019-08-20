59e78291bdec4e769b436f514287d2581cc22ab0
Javad Bargamadi <javadbargamadi1375@gmail.com>
2019-08-20 22:25:14 +0430

    main xml and two other dest activities are completed

--- app/src/main/java/com/sematecjavaproject/imdbonlineoffline/MainActivity.java
+++ app/src/main/java/com/sematecjavaproject/imdbonlineoffline/MainActivity.java
@@ -1,8 +1,16 @@
 package com.sematecjavaproject.imdbonlineoffline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        TextView textViewOpenDrawer = findViewById(R.id.txtOpenDrawer);
+        final TextView textViewOnlineSearch = findViewById(R.id.txtOnlineSearch);
+        final TextView textViewOfflineSearch = findViewById(R.id.txtOfflineSearch);
+
+        textViewOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                drawerLayout.openDrawer(GravityCompat.START);
+
+                textViewOnlineSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent onlineIntent = new Intent(MainActivity.this, OnlineSearch.class);
+                        startActivity(onlineIntent);
+                    }
+                });
+
+                textViewOfflineSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Intent offlineIntent = new Intent(MainActivity.this, OfflineSearch.class);
+                        startActivity(offlineIntent);
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sematecjavaproject/imdbonlineoffline/OnlineSearch.java
@@ -0,0 +1,14 @@
+package com.sematecjavaproject.imdbonlineoffline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnlineSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_search);
+    }
+}
